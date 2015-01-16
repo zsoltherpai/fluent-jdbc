e8bdee3d87ac3da180d6f1d7d5dd2895f2c92b87
mrcellux <zsolt.herpai@gmail.com>
2015-01-16 10:42:38 +0100

    unused code

--- src/main/java/org/fluentjdbc/internal/support/Lists.java
+++ src/main/java/org/fluentjdbc/internal/support/Lists.java
@@ -3,10 +3,6 @@ package org.fluentjdbc.internal.support;
 import java.util.*;
 
 public class Lists {
-    public static <T> List<T> of(T element1, T element2) {
-        return copyOf(Arrays.asList(element1, element2));
-    }
-
     public static <T> List<T> copyOf(Collection<T> collection) {
         return Collections.unmodifiableList(new ArrayList<>(collection));
     }
