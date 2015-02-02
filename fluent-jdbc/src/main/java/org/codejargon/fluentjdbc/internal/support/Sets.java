9999918f07b41d1ff71e8e1e15843d802b01b565
mrcellux <zsolt.herpai@gmail.com>
2015-02-02 15:02:57 +0100

    empty set optimization (used more heavily in tx logic)

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/support/Sets.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/support/Sets.java
@@ -6,17 +6,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Sets {
+    private static final Set<?> emptySet = Collections.unmodifiableSet(new HashSet<>());
+    
+    @SuppressWarnings("unchecked")
     public static <T> Set<T> immutableOf() {
-        return Collections.unmodifiableSet(new HashSet<>());
+        return (Set<T>) emptySet;
     }
 
     public static <T> Set<T> immutableOf(T[] values) {
-        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(values)));
+        return values.length > 0 ? Collections.unmodifiableSet(new HashSet<>(Arrays.asList(values))) : immutableOf();
     }
 
     public static <T> Set<T> merge(Set<T> set1, Set<T> set2) {
         Set<T> merged = new HashSet<>(set1);
         merged.addAll(set2);
         return Collections.unmodifiableSet(merged);
+    }
+
+    public static void main(String[] args) {
+        Set<String> strs = immutableOf();
+        System.out.println(strs.size());
     }
 }
