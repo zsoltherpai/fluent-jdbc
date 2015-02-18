fa99bd12fa4fcb770a1b886cda6a51aa1fa49d03
mrcellux <zsolt.herpai@gmail.com>
2015-02-18 22:56:55 +0100

    marking functional interface

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/QueryRunnerPreparedStatement.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/QueryRunnerPreparedStatement.java
@@ -3,6 +3,7 @@ package org.codejargon.fluentjdbc.internal.query;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface QueryRunnerPreparedStatement<T> {
     T run(PreparedStatement c) throws SQLException;
 }
