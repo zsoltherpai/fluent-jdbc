813053412704c01b0fc9dbfb05eaf2c02de5da66
mrcellux <zsolt.herpai@gmail.com>
2015-02-19 22:29:22 +0100

    simplification

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/ParamAssigner.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/ParamAssigner.java
@@ -9,6 +9,9 @@ import java.util.List;
 import java.util.Map;
 
 class ParamAssigner {
+    private static final ParamSetter defaultParamSetter =
+            (param, statement, index) -> statement.setObject(index, param);
+    
     private final Map<Class, ParamSetter> paramSetters;
 
     ParamAssigner(Map<Class, ParamSetter> paramSetters) {
@@ -25,10 +28,10 @@ class ParamAssigner {
 
     private void assignParam(PreparedStatement statement, Integer index, Object param) {
         try {
-            if (param == null) {
-                assignNull(statement, index);
-            } else {
+            if (param != null) {
                 assignNonNull(param, statement, index);
+            } else {
+                assignNull(statement, index);
             }
         } catch (SQLException e) {
             throw new FluentJdbcSqlException(String.format("Error assigning parameter index %s, object %s", index, param != null ? param.getClass().getName() : "null"), e);
@@ -46,10 +49,11 @@ class ParamAssigner {
 
     @SuppressWarnings("unchecked")
     private void assignNonNull(Object param, PreparedStatement statement, Integer index) throws SQLException {
-        if (paramSetters.containsKey(param.getClass())) {
-            paramSetters.get(param.getClass()).set(param, statement, index);
-        } else {
-            statement.setObject(index, param);
-        }
+        paramSetter(param).set(param, statement, index);
+    }
+    
+    private ParamSetter paramSetter(Object param) {
+        ParamSetter customSetter = paramSetters.get(param.getClass());
+        return customSetter != null ? customSetter : defaultParamSetter;
     }
 }
