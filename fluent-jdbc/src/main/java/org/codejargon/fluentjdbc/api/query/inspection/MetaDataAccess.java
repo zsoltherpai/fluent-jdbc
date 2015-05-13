8aef60e476ad68f4384170a1efa4f960b9e63975
mrcellux <zsolt.herpai@gmail.com>
2015-05-13 21:44:56 +0200

    allows SQLException to be thrown in functional interfaces

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/api/query/inspection/MetaDataAccess.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/api/query/inspection/MetaDataAccess.java
@@ -1,8 +1,9 @@
 package org.codejargon.fluentjdbc.api.query.inspection;
 
 import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 @FunctionalInterface
 public interface MetaDataAccess<T> {
-    T access(DatabaseMetaData meta);
+    T access(DatabaseMetaData meta) throws SQLException;
 }
--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/api/query/inspection/MetaDataResultSet.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/api/query/inspection/MetaDataResultSet.java
@@ -3,8 +3,9 @@ package org.codejargon.fluentjdbc.api.query.inspection;
 
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @FunctionalInterface
 public interface MetaDataResultSet {
-    ResultSet select(DatabaseMetaData metaData);
+    ResultSet select(DatabaseMetaData metaData) throws SQLException;
 }
