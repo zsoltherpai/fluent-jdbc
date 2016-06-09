dbcfd4282e7046b3db63f389b65ea7ce81ff68d7
zsoltherpai <zsolt.herpai@gmail.com>
2016-06-09 09:48:10 +0200

    named parameter with collections caching bug

--- fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/namedparameter/NamedTransformedSqlFactory.java
+++ fluent-jdbc/src/main/java/org/codejargon/fluentjdbc/internal/query/namedparameter/NamedTransformedSqlFactory.java
@@ -4,41 +4,70 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class NamedTransformedSqlFactory {
-    private final Map<String, NamedTransformedSql> namedParamSqlCacheNoCollections;
-    private final Map<Map<String, Integer>, NamedTransformedSql> namedParamCacheWithCollections;
+    private final Map<CacheKey, NamedTransformedSql> namedTransformedSqls;
 
     public NamedTransformedSqlFactory() {
-        this.namedParamSqlCacheNoCollections = Collections.synchronizedMap(new WeakHashMap<>());
-        this.namedParamCacheWithCollections = Collections.synchronizedMap(new WeakHashMap<>());
+        this.namedTransformedSqls = Collections.synchronizedMap(new WeakHashMap<>());
     }
 
     public NamedTransformedSql create(String sql, Map<String, ?> namedParams) {
-        if (hasCollection(namedParams)) {
-            Map<String, Integer> cacheKey = collectionParamCacheKey(namedParams);
-            NamedTransformedSql namedTransformedSql = namedParamCacheWithCollections.get(cacheKey);
-            if(namedTransformedSql == null) {
-                namedTransformedSql = NamedTransformedSql.forSqlAndParams(sql, namedParams);
-                namedParamCacheWithCollections.put(cacheKey, namedTransformedSql);
-            }
-            return namedTransformedSql;
-        } else {
-            NamedTransformedSql namedTransformedSql = namedParamSqlCacheNoCollections.get(sql);
-            if (namedTransformedSql == null) {
-                namedTransformedSql = NamedTransformedSql.forSqlAndParams(sql, namedParams);
-                namedParamSqlCacheNoCollections.put(sql, namedTransformedSql);
-            }
-            return namedTransformedSql;
+        CacheKey cacheKey = cacheKey(sql, namedParams);
+        NamedTransformedSql namedTransformedSql = namedTransformedSqls.get(cacheKey);
+        if (namedTransformedSql == null) {
+            namedTransformedSql = NamedTransformedSql.forSqlAndParams(sql, namedParams);
+            namedTransformedSqls.put(cacheKey, namedTransformedSql);
         }
+        return namedTransformedSql;
     }
 
     public static boolean hasCollection(Map<String, ?> namedParams) {
         return namedParams.values().stream().filter(value -> value instanceof Collection).findFirst().isPresent();
     }
 
-    private Map<String, Integer> collectionParamCacheKey(Map<String, ?> namedParams) {
-        return Collections.unmodifiableMap(new TreeMap<>(namedParams).entrySet().stream()
-                        .filter(entry -> entry.getValue() instanceof Collection)
-                        .collect(Collectors.toMap(Map.Entry::getKey, e -> ((Collection) e.getValue()).size()))
-        );
+    private CacheKey cacheKey(String sql, Map<String, ?> namedParams) {
+        Optional<Map<String, Integer>> paramCounts = hasCollection(namedParams) ?
+                Optional.of(
+                        Collections.unmodifiableMap(new TreeMap<>(namedParams).entrySet().stream()
+                                .collect(
+                                        Collectors.toMap(
+                                                Map.Entry::getKey,
+                                                e -> (e.getValue() instanceof Collection) ?
+                                                        ((Collection) e.getValue()).size() :
+                                                        1
+                                        )
+                                )
+                        )
+                ) :
+                Optional.empty();
+        return new CacheKey(sql, paramCounts);
+    }
+
+    private static class CacheKey {
+        private final String sql;
+        private final Optional<Map<String, Integer>> paramCounts;
+
+        private CacheKey(String sql, Optional<Map<String, Integer>> paramCounts) {
+            this.sql = sql;
+            this.paramCounts = paramCounts;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            CacheKey that = (CacheKey) o;
+
+            if (!sql.equals(that.sql)) return false;
+            return paramCounts.equals(that.paramCounts);
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = sql.hashCode();
+            result = 31 * result + paramCounts.hashCode();
+            return result;
+        }
     }
 }
